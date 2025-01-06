6f740d5d33b23b3bc385cafa66a89a54bb78d43a
mhj <mohammadhoseinjabbari1383wa@gmail.com>
2025-01-06 17:02:39 +0330

    add GuessGame project

---
+++ guess_game_reversed.py
@@ -0,0 +1,37 @@
+import random
+
+guess_lst = []
+count = 0
+
+
+def new_guess(start_of_bound, end_of_bound):
+    return random.randint(start_of_bound, end_of_bound)
+
+
+num = int(input("input the number [and AI gonna guess that!] :  "))
+end_of_bound = 100
+start_of_bound = 0
+guess = new_guess(start_of_bound, end_of_bound)
+
+while guess != num:
+    if guess in guess_lst:
+        guess = new_guess(start_of_bound, end_of_bound)
+
+    print(f"I think your number is {guess}")
+    guess_lst.append(guess)
+    count += 1
+
+    txt = input("what should I do? go higher or lower? (h/l): ")
+
+    if txt == "l":
+        end_of_bound = guess - 1
+        guess = new_guess(start_of_bound, end_of_bound)
+
+    elif txt == "h":
+        start_of_bound = guess + 1
+        guess = new_guess(start_of_bound, end_of_bound)
+
+    else:
+        print("you entered a wrong input. try again")
+
+print(f"your number was {guess}!!")
