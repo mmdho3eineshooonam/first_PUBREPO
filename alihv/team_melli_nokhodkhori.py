e59eeec05a2b806ce37e0b71ef735a054eac48f8
mhj <mohammadhoseinjabbari1383wa@gmail.com>
2025-03-11 13:08:47 +0330

    adding nokhod khori to alihv folder

---
+++ alihv/team_melli_nokhodkhori.py
@@ -0,0 +1,13 @@
+x = 0
+time_elapsed_list = []
+team_members = int(input("enter the number of team members inside the shop\n(number should be 1/2/3): "))
+price : int = input("please enter the prices\n(separate a & b & c prices with space)\n").split()
+for _ in range(team_members):
+    time = input(
+        f"enter the Check-in and check-out times for person {_ + 1} \n(separate Check-in and check-out times with space)\n").split()
+    elapsed = int(time[1]) - int(time[0])
+    time_elapsed_list.append(elapsed)
+
+for index in range(len(time_elapsed_list)):
+    x += int(price[index]) * int(time_elapsed_list[index])
+print(x)
