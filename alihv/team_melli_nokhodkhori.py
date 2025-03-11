1715e88e9bab9df2d64592abe60cdb2f0ee1756b
mhj <mohammadhoseinjabbari1383wa@gmail.com>
2025-03-11 13:11:10 +0330

    cleaning up the nokhod khori code

--- alihv/team_melli_nokhodkhori.py
+++ alihv/team_melli_nokhodkhori.py
@@ -1,7 +1,9 @@
-x = 0
+result = 0
 time_elapsed_list = []
 team_members = int(input("enter the number of team members inside the shop\n(number should be 1/2/3): "))
+
 price : int = input("please enter the prices\n(separate a & b & c prices with space)\n").split()
+
 for _ in range(team_members):
     time = input(
         f"enter the Check-in and check-out times for person {_ + 1} \n(separate Check-in and check-out times with space)\n").split()
@@ -9,5 +11,6 @@ for _ in range(team_members):
     time_elapsed_list.append(elapsed)
 
 for index in range(len(time_elapsed_list)):
-    x += int(price[index]) * int(time_elapsed_list[index])
-print(x)
+    result += int(price[index]) * int(time_elapsed_list[index])
+
+print(result)
